61ac578378deac24aec01a1c2b4d3d704230117b
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-07-24 22:29:21 +0200

    More types are added with link to documentation

--- 02.3.Trackball.py
+++ 02.3.Trackball.py
@@ -28,25 +28,52 @@
 # Author : Mariwan Jalal  mariwan.jalal@gmail.com                  #
 ####################################################################
 
-import os,sys
+import os
+import sys
 import FreeCAD as App
 import FreeCADGui as Gui
 import pivy.coin as coin
 
-#Great Example how you can use this to rotate objects :)
+# Great Example how you can use this to rotate objects :)
+
+
 def Trackball():
 
     root = coin.SoSeparator()
 
-    root.addChild(coin.SoDirectionalLight()) # child 1
-    root.addChild(coin.SoTrackballManip())   # child 2
+    root.addChild(coin.SoDirectionalLight())  # child 1
+    # child 2     # You can choose other types .. Look at the note bellow the code
+    root.addChild(coin.SoTrackballManip())
 
     myMaterial = coin.SoMaterial()
     myMaterial.diffuseColor = (1.0, 0.0, 0.0)
     root.addChild(myMaterial)
     root.addChild(coin.SoCone())
 
-
     view = Gui.ActiveDocument.ActiveView
     sg = view.getSceneGraph()
     sg.addChild(root)
+
+    """
+    Note:
+    There are many other kind of this manupulation widget. Please choose the following classes to get other types.add()
+    
+    #link : http://webcache.googleusercontent.com/search?q=cache:9kTXW4u5hIkJ:www-evasion.inrialpes.fr/people/Francois.Faure/doc/inventor_doc.con3d.org/Coin/group__manips.html+&cd=12&hl=sv&ct=clnk&gl=se&client=firefox-b-d
+
+    coin.SoCenterballManip()   	        #The SoCenterballManip wraps an SoCenterballDragger for convenience. More...
+    coin.SoClipPlaneManip()      	    #The SoClipPlaneManip class is used to manipulate clip planes. More...
+    coin.SoDirectionalLightManip()  	#The SoDirectionalLightManip class is used to manipulate SoDirectionalLight nodes. More...
+    coin.SoHandleBoxManip()         	#The SoHandleBoxManip class wraps an SoHandleBoxDragger for manipulating a transformation. More...
+    coin.SoJackManip()              	#The SoJackManip wraps an SoJackDragger for convenience. More...
+    coin.SoPointLightManip()       	    #The SoPointLightManip class is used to manipulate point light nodes. More...
+    coin.SoSpotLightManip()         	#The SoSpotLightManip class is used to manipulate spot light nodes. More...
+    coin.SoTabBoxManip()            	#The SoTabBoxManip class wraps an SoTabBoxDragger. More...
+    coin.SoTrackballManip()    	        # The SoTrackballManip wraps an SoTrackballDragger for convenience. More...
+    coin.SoTransformBoxManip()       	#The SoTransformBoxManip wraps an SoTransformBoxDragger for convenience. More...
+    coin.SoTransformManip()         	#The SoTransformManip class is used to manipulate transformations. More...
+    coin.SoTransformerManip()           #The SoTransformerManip wraps an SoTransformerDragger for convenience
+
+    
+    
+    
+    """
